9d566b50773fe4ebe00153b5274209d23b5eba45
Eva <145874312+EvaCanary@users.noreply.github.com>
2024-02-09 22:29:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+let post = {
+    title: '',
+    text: '',
+};
+const postTitleInputNode = document.querySelector('.js-post-title-input');
+const postTextInputNode = document.querySelector('.js-post-text-btn');
+const newPostBtnNode = document.querySelector('.js-new-post-btn')
+const postsNode = document.querySelector('.js-posts');
+
+newPostBtnNode.addEventListener('click', function(){
+
+ const postFromUser = getPostFromUser();
+   
+ setPost(postFromUser);
+
+ renderPost();
+});
+   
+ //получить данные из поля вводы
+
+function getPostFromUser() {
+    const title = postTitleInputNode.value;
+    const text = postTextInputNode.value;
+    return {
+        title: title,
+        text: text
+    };
+}
+ //сохранить пост
+
+function setPost(post) {
+    post = newPost;
+}
+function getPost() {
+    return post;
+}
+
+//отобразить пост
+
+function renderPost() {
+const post = getPost();
+
+const postHTML =`
+<div class='post'>
+   <p class='post__title'>${post.title}</p>
+   <p class='post__text'>${post.text}</p>
+</div>
+`;
+
+postsNode.innerHTML = postHTML;
+}
