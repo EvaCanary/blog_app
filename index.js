87ff5c0b7f76f1a27784dd9f54cc9edb66ab2dbd
Eva <145874312+EvaCanary@users.noreply.github.com>
2024-02-17 23:32:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
+
 let posts = [];
+const TITLE_VALIDATION_LIMIT = 20;
+const TEXT_VALIDATION_LIMIT = 100;
 
 const postTitleInputNode = document.querySelector('.js-post-title-input');
 const postTextInputNode = document.querySelector('.js-post-text-input');
 const newPostBtnNode = document.querySelector('.js-new-post-btn');
 const postsNode = document.querySelector('.js-posts');
+const validationMessage = document.querySelector(".js-validation-message"); // Correct selector
 
-newPostBtnNode.addEventListener('click', function(){
-
- const postFromUser = getPostFromUser();
-   
- setPost(postFromUser);
-
- renderPost();
+newPostBtnNode.addEventListener('click', function() {
+    validation(); // Call validation before adding a new post
+    const postFromUser = getPostFromUser();    
+    addPost(postFromUser);
+    renderPosts();
 });
-   
- //получить данные из поля вводы
+
+postTitleInputNode.addEventListener("input", validation);
+postTextInputNode.addEventListener("input", validation);
+
+function validation(){
+   const titleLen = postTitleInputNode.value.length;
+   const textLen = postTextInputNode.value.length;
+
+   if (titleLen > TITLE_VALIDATION_LIMIT){
+     validationMessage.innerText = `Длинна заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов`; // Corrected message
+     validationMessage.classList.remove("validationMessage_hidden");
+     return;
+   }
+
+   if (textLen > TEXT_VALIDATION_LIMIT){
+    validationMessage.innerText = `Длинна текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов`; // Corrected message
+    validationMessage.classList.remove("validationMessage_hidden");
+    return;
+  }
+  validationMessage.classList.add("validationMessage_hidden");
+}
 
 function getPostFromUser() {
     const title = postTitleInputNode.value;
     const text = postTextInputNode.value;
+
     return {
         title: title,
-        text: text
-    };
-}
- //сохранить пост
-
-function setPost(postFromUser) {
-    posts.push(postFromUser);
-}
-function getPost() {
-    return posts;
+        text: text,
+    };    
 }
 
- //отобразить пост
+function addPost({ title, text }) {
+    posts.push({
+        title,
+        text,
+    });
+}
 
-function renderPost() {
-const posts = getPost();
+function renderPosts() {
+    let postsHTML = '';
+    posts.forEach(post => {
+        postsHTML += ` 
+        <div class='post'>
+        <p class='post__title'>${post.title}</p>
+        <p class='post__text'>${post.text}</p>
+        </div>`;
+    });
 
-let postsHTML = '';
-posts.forEach((post) => {
-    postsHTML += `
-    <div class='post'>
-       <p class='post__title'>${post.title}</p>
-       <p class='post__text'>${post.text}</p>
-    </div>
-    `
-} );
-postsNode.innerHTML = postsHTML;
+    postsNode.innerHTML = postsHTML;
 }
